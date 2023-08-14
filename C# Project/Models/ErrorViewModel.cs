1b5b56b7544363ebc807af41242cdd6aed105d06
johna <johna@ANAKTAS>
2023-08-14 17:47:11 +0200

    Add project files.

---
+++ C# Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
